49baa103478f50a842b7d5c619c236a4e6424718
Cristhian <cristhianE2018>
2021-06-14 17:48:10 -0500

    Modificación del archivo asociacion dos

--- src/components/asociaciones/asociacion2.vue
+++ src/components/asociaciones/asociacion2.vue
@@ -1,1 +1,7 @@
-<h1>Hola</h1>
+<template>
+    <v-container>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Tabla de las asociaciones de la zona sur del estado de Veracruz
+        </h1>
+    </v-container>
+</template>
