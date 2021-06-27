839393ef9398d54bcc9ffa67eb9c4ff93237db2b
Erick <egarcia@lania.edu.mx>
2021-06-27 16:18:21 -0500

    Apenas hice la practica equi de

---
+++ src/components/asociaciones/ErICK.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-container>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Tabla de las asociaciones de la zona sur del estado de Veracruz
+        </h1>
+    </v-container>
+</template>
